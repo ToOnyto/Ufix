1264b352cde3ace35473b80e1fe4dc1b5494086f
Antoine Maurel <antoinemaurel6@gmail.com>
2019-10-12 02:55:42 +0200

    first page done

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,3 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello Bulma!');
+document.addEventListener('DOMContentLoaded', function () {
+  new TypeIt('#txtRun', {
+    strings: ["Hello !", "Bienvenue sur UFix !"],
+    speed: 100,
+    breakLines: false,
+    waitUntilVisible: true
+  }).go();
 });
